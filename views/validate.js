f44d85bbbe840c7d45b83b715a6fd17a50b32178
Andrew Horvath <skwerl@student.fullsail.edu>
2022-10-19 22:00:36 -0500

    trying to get the EJS in the html to work, no such luck.

--- views/validate.js
+++ views/validate.js
@@ -25,30 +25,30 @@ module.exports = (req, res, next) => {
     if(fName != regex_Name){
         errors.fnameMsg = "First Name is required";
     }
-    if (lName != regex_Name){
-        errors.lnameMsg = "Last Name is required";
-    }
-    if (email != regex_Email){
-        errors.emailMsg = "Email is required";
-    }
-    if (address != regex_Address){
-        errors.addressMsg = "Invalid Address!"
-    }
-    if (city != regex_City){
-        errors.cityMsg = "Invalid City!"
-    }
-    if (state != regex_State){
-        errors.stateMsg = "Invalid State!"
-    }
-    if (zip != regex_Zip){
-        errors.zipMsg = "Invalid Zip!"
-    }
-    if (password != regex_Password){
-        errors.passwordMsg = "Invalid Password! Must be at least 7 characters and start with a letter";
-    }
-    if (passMatch != password){
-        errors.passMatchMsg = "Passwords must match";
-    }
+    // if (lName != regex_Name){
+    //     errors.lnameMsg = "Last Name is required";
+    // }
+    // if (email != regex_Email){
+    //     errors.emailMsg = "Email is required";
+    // }
+    // if (address != regex_Address){
+    //     errors.addressMsg = "Invalid Address!"
+    // }
+    // if (city != regex_City){
+    //     errors.cityMsg = "Invalid City!"
+    // }
+    // if (state != regex_State){
+    //     errors.stateMsg = "Invalid State!"
+    // }
+    // if (zip != regex_Zip){
+    //     errors.zipMsg = "Invalid Zip!"
+    // }
+    // if (password != regex_Password){
+    //     errors.passwordMsg = "Invalid Password! Must be at least 7 characters and start with a letter";
+    // }
+    // if (passMatch != password){
+    //     errors.passMatchMsg = "Passwords must match";
+    // }
 
   req.errors = errors;
   next();
